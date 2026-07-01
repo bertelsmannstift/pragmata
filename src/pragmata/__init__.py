d267449a273088d476027f5103c8fb093c66783f
saschagobel <sascha.gbl@googlemail.com>
2026-07-01 13:43:59 +0200

    feat: expose eval namespace

--- src/pragmata/__init__.py
+++ src/pragmata/__init__.py
@@ -5,9 +5,9 @@ Only curated, stable symbols should be exposed here.
 
 import logging
 
-from . import annotation, querygen
+from . import annotation, eval, querygen
 from .api import get_version
 
-__all__ = ["annotation", "get_version", "querygen"]
+__all__ = ["annotation", "eval", "get_version", "querygen"]
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
