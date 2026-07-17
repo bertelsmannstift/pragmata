db94813d379665261353ded166282ae06ab252d9
saschagobel <sascha.gbl@googlemail.com>
2026-07-17 15:00:50 +0200

    feat: expose predict_labels from public eval namespace

--- src/pragmata/eval/__init__.py
+++ src/pragmata/eval/__init__.py
@@ -1,5 +1,6 @@
 """Public evaluation namespace."""
 
+from pragmata.api.eval import predict_labels as predict_labels
 from pragmata.api.eval import train_evaluator as train_evaluator
 
-__all__ = ["train_evaluator"]
+__all__ = ["predict_labels", "train_evaluator"]
